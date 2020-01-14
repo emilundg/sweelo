e4d883390b01c0c90d84e6344007824dd3a7ca1e
emilundg <lundgrenemil@hotmail.com>
2020-01-14 16:31:12 +0100

    ADD multiple file bundling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
 import './style.css';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join([
@@ -11,6 +13,11 @@ function component() {
     element
         .classList
         .add('text__red');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
