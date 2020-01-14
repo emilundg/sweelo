feb6c0cd844fd8de5edc1d123b47fd23f4301f1c
emilundg <lundgrenemil@hotmail.com>
2020-01-14 16:38:36 +0100

    ADD clean of dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
         app: './src/index.js',
         print: './src/print.js'
     },
-    plugins: [new HtmlWebpackPlugin({title: 'Sweelo'})],
+    plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({title: 'Sweelo'})],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
