a5f8342f5bb2910a65051ffd7ed38fb247e972aa
emilundg <lundgrenemil@hotmail.com>
2020-05-28 11:57:36 +0200

    ADD state

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            header: "Sweelo"
+        }
+    }
     render() {
+        const {header} = this.state;
         return (
             <div>
-                <h1>Header</h1>
+                <h1>{header}</h1>
             </div>
         );
     }
