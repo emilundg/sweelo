0c701cbeb52927e31707d1ad238ad5fc851bdd61
emilundg <lundgrenemil@hotmail.com>
2020-05-28 10:38:43 +0200

    CHANGE to package files

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
         app: './src/index.js',
         print: './src/print.js'
     },
-    plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({title: 'Sweelo'})],
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({title: 'Sweelo'}),
+        new VueLoaderPlugin()
+    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
@@ -17,6 +21,9 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            }, {
+                test: /\.vue$/,
+                loader: 'vue-loader'
             }
         ]
     }
