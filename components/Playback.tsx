39eb63bfb7aa704b6850dd87fd54e65825de1639
emilundg <lundgrenemil@hotmail.com>
2020-05-29 16:05:37 +0200

    DISPLAY token

--- components/Playback.tsx
+++ components/Playback.tsx
@@ -5,6 +5,7 @@ import {setAccessToken} from "../actions/authenticateActions";
 
 export interface PlaybackProps {
     dispatch : any;
+    token : string;
 }
 
 class Playback extends React.Component < PlaybackProps, {} > {
@@ -14,22 +15,25 @@ class Playback extends React.Component < PlaybackProps, {} > {
                 .location
                 .hash
                 .split('=')[1]
-            console.log(token)
             this
                 .props
                 .dispatch(setAccessToken(token))
         }
     }
     render() {
+        const {token} = this.props;
         return (
             <div>
                 <h1>Playback route</h1>
                 <p>test</p>
+                <p>
+                    {token}
+                </p>
             </div>
         );
     }
 }
 
 const mapDispatchToProps = (dispatch : any) => ({setAccessToken, dispatch});
-const mapStateToProps = (state : any) => ({});
+const mapStateToProps = (state : any) => ({token: state.authenticate.token});
 export default connect(mapStateToProps, mapDispatchToProps)(Playback);
